8cdfcfc5fa3cafc36fafd3565cfaf504e0100c4f
Ivan Ramiro Laruta Espinal <ivan.laruta@bolivia.toyosa.com>
2017-04-13 15:46:21 -0400

    ya funca

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      * @var string
      */
 
-    protected $redirectTo = '/hola';
+    protected $redirectTo = '/inicial';
 
     /**
      * Create a new controller instance.
