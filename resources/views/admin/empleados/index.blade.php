434c41ab1c30e6c15e24fd10c4fd3f56bf36829e
Ivan Ramiro Laruta Espinal <ivan.laruta@bolivia.toyosa.com>
2017-04-28 18:42:27 -0400

    usuario pass2

--- resources/views/admin/empleados/index.blade.php
+++ resources/views/admin/empleados/index.blade.php
@@ -3,6 +3,7 @@
 
 @section('content')
 @section('ventana','Lista de empleados')
+<div class="row">
     	<div class="row">
   			<div class="col-md-12">
 				<a href="{{ route('empleados.create')}}" class="btn btn-success">Registrar nuevo empleado</a><hr>
@@ -31,7 +32,9 @@
 
 								<td>
 								<div class="row">
-									<a href="{{ route('empleados.edit',$usu -> ci )}}" class="btn btn-warning" title="Modificar"><span class="fa fa-edit "></span></a> 
+									
+									<a href="{{ route('empleados.edit',$usu -> ci )}}" class="btn btn-warning" title="Modificar"><span class="fa fa-edit "></span></a>
+
 									<a href="{{ route('empleados.baja',$usu -> ci )}}" onclick ="return confirm('¿Desea Dar de baja?')" class="btn btn-danger" title="Eliminar"><span class="fa fa-trash-o"></span></a>  
 								</div>
 								</td>
@@ -43,7 +46,7 @@
  --}} 			</div>
  			</div>
 		</div>
-
+</div>
 @endsection
 
 @section('scripts')
--- resources/views/template/partials/dash_ope_slide.blade.php
+++ resources/views/template/partials/dash_ope_slide.blade.php
@@ -1,13 +1,13 @@
  <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav">
-                    <li >
-                        <a href="{{ route('visitas.index')}}"><i class="fa fa-table"></i> Visitas en curso</a>
-                    </li>
+                    
                     <li>
                         <a href="{{ route('visitas.create')}}"><i class="fa fa-sign-out"></i> Registrar nueva visita</a>
                     </li>
-                    
+                    <li >
+                        <a href="{{ route('visitas.index')}}"><i class="fa fa-table"></i> Visitas en curso</a>
+                    </li>
                      <li>
                         <a href="javascript:;" data-toggle="collapse" data-target="#demo2"><i class="fa fa-bar-chart-o"></i> Reportes <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="demo2" class="collapse in">
